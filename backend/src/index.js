f429e23636fbfc1b3a9061ade2dc119395223ed7
srjonjoy <ghoshsrinjoy2@gmail.com>
2025-09-16 22:27:23 +0530

    start deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,12 +4,14 @@ import ConnectDB from "./db/indexdb.js"
 import cookieparser from "cookie-parser"
 import authUser from "./routes/auth.routes.js"
 import messageRouter from "./routes/message.routes.js"
+import path from "path"
 import cors from "cors"
 
 
 
 dotenv.config()
 const app=express()
+const __dirname=path.resolve()
 app.use(cookieparser())
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
@@ -28,6 +30,14 @@ app.use(
 );
 app.use("/api/v1/auth",authUser)
 app.use("/api/v1/message",messageRouter)
+// make ready for deployment
+if(process.env.NODE_ENV==="production"){
+  app.use(express.static(path.join(__dirname,"..frontend/dist")))
+  app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"..frontend","dist","index.html"))
+  })
+
+}
 const port=process.env.PORT||3000
 
 ConnectDB()
